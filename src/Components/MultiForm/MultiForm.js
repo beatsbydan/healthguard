78010105f7ff9ee4c65ff2f3ff3e585a0edd8b55
Onyeachonam Daniel <danielonyeachonam2020@gmail.com>
2023-08-09 09:28:28 +0100

    Done

--- src/Components/MultiForm/MultiForm.js
+++ src/Components/MultiForm/MultiForm.js
@@ -1,8 +1,6 @@
 import React, { useContext } from 'react';
 import './MultiForm.css'
 import FormContext from '../../Contexts/FormContext/FormContext';
-import {BsFillChatFill} from 'react-icons/bs'
-import { Link } from 'react-router-dom';
 import Logo from '../Logo/Logo';
 const MultiForm = () => {
     const ctx = useContext(FormContext)
@@ -13,10 +11,6 @@ const MultiForm = () => {
         <section className='multiForm'>
             <div className="top">
                 <Logo/>
-                {/* <div className="letsChat">
-                    <BsFillChatFill color='#A3C6C4' size={25}/>
-                    <Link to={'/chat'}>CHAT</Link>
-                </div> */}
             </div>
             <form action="" onSubmit={handleSubmit}>
                 {ctx.getPage()}
